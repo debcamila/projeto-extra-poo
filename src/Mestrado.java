b796c3252ea04ee2c1e055b7d16f409a606a96fe
debcamila <camiladeborah72@gmail.com>
2019-06-03 23:42:15 -0300

    classe do aluno mestrado

---
+++ src/Mestrado.java
@@ -0,0 +1,7 @@
+
+public class Mestrado extends Aluno{
+	
+	public Mestrado(String nome, int faltas, double notaPrimeira, double notaSegunda) {
+		super(nome, faltas, notaPrimeira, notaSegunda);
+	}
+}
