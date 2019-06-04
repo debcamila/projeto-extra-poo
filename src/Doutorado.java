1f51709fdb372261bf20e1103ddaeac238e4d7f4
debcamila <camiladeborah72@gmail.com>
2019-06-03 23:41:38 -0300

    classe do aluno doutorado

---
+++ src/Doutorado.java
@@ -0,0 +1,8 @@
+
+public class Doutorado extends Aluno{
+
+	public Doutorado(String nome, int faltas, double notaPrimeira, double notaSegunda) {
+		super(nome, faltas, notaPrimeira, notaSegunda);
+	}
+
+}
